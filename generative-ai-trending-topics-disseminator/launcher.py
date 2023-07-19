e5a8d34050382af4bb75c65f1ea7c51ca5132cd1
Aditi Bhatia <aditibhatia@Aditis-MacBook-Pro.local>
2023-07-20 00:49:49 +0530

    adding Dockerfile launcher.py requirements.txt

---
+++ generative-ai-trending-topics-disseminator/launcher.py
@@ -0,0 +1,12 @@
+# launcher.py
+import os
+import subprocess
+
+script = os.getenv('SCRIPT_NAME')
+
+if script == 'main_daily':
+    subprocess.run(['python', 'main_daily.py'])
+elif script == 'main_monthly':
+    subprocess.run(['python', 'main_monthly.py'])
+else:
+    print('Invalid SCRIPT_NAME. Please set SCRIPT_NAME to either "main_daily" or "main_monthly".')
