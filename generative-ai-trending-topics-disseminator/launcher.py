f7e8b6212b3e80a73eee9ed476d4d90b4c0a4647
Aditi Bhatia <aditibhatia@Aditis-MacBook-Pro.local>
2023-07-20 01:19:09 +0530

    changed launcher.py

--- generative-ai-trending-topics-disseminator/launcher.py
+++ generative-ai-trending-topics-disseminator/launcher.py
@@ -1,12 +1,14 @@
 # launcher.py
+
 import os
 import subprocess
 
 script = os.getenv('SCRIPT_NAME')
 
 if script == 'main_daily':
-    subprocess.run(['python', 'main_daily.py'])
+    subprocess.run(['python', '/app/main_daily.py'])
 elif script == 'main_monthly':
-    subprocess.run(['python', 'main_monthly.py'])
+    subprocess.run(['python', '/app/main_monthly.py'])
 else:
     print('Invalid SCRIPT_NAME. Please set SCRIPT_NAME to either "main_daily" or "main_monthly".')
+
